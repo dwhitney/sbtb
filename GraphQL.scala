ef6047b5eeb6998da57c5d2710d97c5480f77152
Dustin Whitney <dustin.whitney@gmail.com>
2016-11-12 09:46:33 -0800

    converting tabs to spaces

--- GraphQL.scala
+++ GraphQL.scala
@@ -9,89 +9,89 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 
 case class User(
-	username: String
+  username: String
 )
 
 case class Avatar(
-	url: String
+  url: String
 , mobileOptimizedURL: String
 )
 
 trait AvatarRepository{
-	def getAvatar(
-		username: String
-	): Option[Avatar]
+  def getAvatar(
+    username: String
+  ): Option[Avatar]
 }
 
 trait UserRepository{
-	def getUser(
-		username: String
-	): Option[User]
+  def getUser(
+    username: String
+  ): Option[User]
 }
 
 
 object GraphQL extends App{
 
-	class Cake extends AvatarRepository 
-		with UserRepository
-	{
+  class Cake extends AvatarRepository 
+    with UserRepository
+  {
 
-		val users = Map(
-			"odersky" -> User("odersky")
-		)
-		val avatars = Map(
-			"odersky" -> Avatar(
-				url = "..."
-			, mobileOptimizedURL = "..."
-			)
-		)
-		def getAvatar(
-			username: String
-		): Option[Avatar] = 
-			avatars.get(username)
-			
+    val users = Map(
+      "odersky" -> User("odersky")
+    )
+    val avatars = Map(
+      "odersky" -> Avatar(
+        url = "..."
+      , mobileOptimizedURL = "..."
+      )
+    )
+    def getAvatar(
+      username: String
+    ): Option[Avatar] = 
+      avatars.get(username)
+      
 
-		def getUser(
-			username: String
-		): Option[User] = 
-			users.get(username)
-	}
+    def getUser(
+      username: String
+    ): Option[User] = 
+      users.get(username)
+  }
 
-	val avatarObj = ObjectType(
-		"Avatar"
-	, fields[Cake, Avatar](
-			Field("url", StringType, resolve = _.value.url)
-		, Field("mobileOptimizedURL"
-				, StringType
-				, resolve = _.value.mobileOptimizedURL
-			)
-		)
-	)
+  val avatarObj = ObjectType(
+    "Avatar"
+  , fields[Cake, Avatar](
+      Field("url", StringType, resolve = _.value.url)
+    , Field("mobileOptimizedURL"
+        , StringType
+        , resolve = _.value.mobileOptimizedURL
+      )
+    )
+  )
 
-	val userObj = deriveObjectType[Cake, User](
-		AddFields(
-				Field("avatar"
-				, OptionType(avatarObj)
-				, resolve = { ctx: Context[Cake, User] => 
-						ctx.ctx.getAvatar(ctx.value.username) 
-				})	
-		)
-	) 
+  val userObj = deriveObjectType[Cake, User](
+    AddFields(
+        Field("avatar"
+        , OptionType(avatarObj)
+        , resolve = { ctx: Context[Cake, User] => 
+            ctx.ctx.getAvatar(ctx.value.username) 
+        })  
+    )
+  ) 
 
-	val queryObj = ObjectType(
-		"Root"
-	, fields[Cake, Unit](
-				Field(
-					"user"
-				, OptionType(userObj)
-				, arguments = Argument("username", StringType) :: Nil
-				, resolve = { ctx => ctx.ctx.getUser((ctx arg "username")) }
-			)
-		)	
-	)
+  val queryObj = ObjectType(
+    "Root"
+  , fields[Cake, Unit](
+        Field(
+          "user"
+        , OptionType(userObj)
+        , arguments = Argument("username", StringType) :: Nil
+        , resolve = { ctx => ctx.ctx.getUser((ctx arg "username")) }
+      )
+    ) 
+  )
 
-	val schema = Schema(queryObj)
-	println(SchemaRenderer.renderSchema(schema))
+  val schema = Schema(queryObj)
+  println(SchemaRenderer.renderSchema(schema))
 
 val query = 
 """{
